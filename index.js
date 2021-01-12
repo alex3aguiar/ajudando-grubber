f83b1061eea863f8e383c9da2a47bfc883ccb24e
Alex Aguiar <alex3aguiar@gmail.com>
2021-01-12 10:30:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors')
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const A_URL_DO_BACK_AQUI = process.env.A_URL_DO_BACK_AQUI;
+const PORT = process.env.PORT ? process.env.PORT : 3000;
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const A_URL_DO_BACK_AQUI  = "url do back aqui"
-app.use('/', createProxyMiddleware({ target: 'A_URL_DO_BACK_AQUI, changeOrigin: true }));
-app.listen(3000);
+app.use(cors())
+app.use('/', createProxyMiddleware({ target: A_URL_DO_BACK_AQUI, changeOrigin: true }));
+
+app.listen(PORT, () => (console.log(`rodando na porta ${PORT}`)));
