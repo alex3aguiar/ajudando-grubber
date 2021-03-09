275830516ae468f5ea122a095792d473b73d3f63
Alex Aguiar <alex3aguiar@gmail.com>
2021-03-09 08:39:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const cors = require('cors')
-const dotenv = require('dotenv');
 
-dotenv.config();
+const argv = require('minimist')(process.argv.slice(2));
 
-const A_URL_DO_BACK_AQUI = process.env.A_URL_DO_BACK_AQUI;
-const PORT = process.env.PORT ? process.env.PORT : 3000;
+
+if (argv.h || argv.help) {
+    console.log([
+        'usage: http-server [path] [options]',
+        '',
+        'options:',
+        '  -p --port    Port to use [8080]',
+        '  -t --target    server target',
+        '  -h --help          Print this list and exit.',
+    ].join('\n'));
+    process.exit();
+}
+
+
+const TARGET = argv.t || argv.target;
+const PORT = argv.p || argv.port || 3392;
+
+if (TARGET === null || TARGET === '') {
+    console.error("define target with -t or --target")
+    process.exit(1);
+}
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 app.use(cors())
-app.use('/', createProxyMiddleware({ target: A_URL_DO_BACK_AQUI, changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: TARGET, changeOrigin: true }));
 
-app.listen(PORT, () => (console.log(`rodando na porta ${PORT}`)));
+app.listen(PORT, () => {
+    console.log(`running at  ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+    console.log(`target to  ${TARGET}`);
+});
